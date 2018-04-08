7827124d9add1305a303e913a8e2012ee9d74b52
Remi Ete <remi.ete@gmail.com>
2018-04-08 17:23:23 +0200

    Added github files and travis scripts

---
+++ .travis-ci.d/compile_and_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ls -la
+
+source dependencies/root/bin/thisroot.sh
+
+mkdir -p build
+cd build
+
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../dependencies/xdrstream -DCMAKE_MODULE_PATH=$PWD/../dependencies/dqm4hep/cmake -DDQMCore_DIR=$PWD/../dependencies/dqm4hep-core -DDQMNet_DIR=$PWD/../dependencies/dqm4hep-net -DDQMOnline_DIR=$PWD/../dependencies/dqm4hep-online -DBUILD_TESTS=ON ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run cmake"
+    exit 1
+fi
+
+make install VERBOSE=1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run make"
+    exit 1
+fi
+
+ldd ./lib/*.so
+ldd ./bin/*
---
+++ .travis-ci.d/install_dependencies.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+ls -la
+mkdir -p dependencies && cd dependencies
+
+# install root
+wget https://root.cern.ch/download/root_v6.10.08.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
+tar -xf root_v6.10.08.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
+source root/bin/thisroot.sh
+root-config --version
+
+# install dqm4hep cmake macros
+git clone https://github.com/dqm4hep/dqm4hep.git
+export DQM4HEP_DIR=$PWD/dqm4hep
+
+# install xdrstream
+git clone https://github.com/dqm4hep/xdrstream.git
+cd xdrstream
+export XDRSTREAM_DIR=$PWD
+mkdir build && cd build
+cmake -DINSTALL_DOC=OFF ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run xdrstream cmake"
+    exit 1
+fi
+
+make install VERBOSE=1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run xdrstream make"
+    exit 1
+fi
+
+cd ../..
+
+# install dqm4hep-core
+git clone https://github.com/dqm4hep/dqm4hep-core.git
+cd dqm4hep-core
+mkdir -p build && cd build
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../../xdrstream -DCMAKE_MODULE_PATH=$PWD/../../dqm4hep/cmake -DBUILD_TESTS=OFF ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-core cmake"
+    exit 1
+fi
+
+make install VERBOSE=1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-core make"
+    exit 1
+fi
+cd ../..
+
+# install dqm4hep-net
+git clone https://github.com/dqm4hep/dqm4hep-net.git
+cd dqm4hep-net
+mkdir -p build && cd build
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../../xdrstream -DCMAKE_MODULE_PATH=$PWD/../../dqm4hep/cmake -DDQMCore_DIR=$PWD/../../dqm4hep-core ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-net cmake"
+    exit 1
+fi
+
+make install VERBOSE=1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-net make"
+    exit 1
+fi
+cd ../..
+
+# install dqm4hep-net
+git clone https://github.com/dqm4hep/dqm4hep-online.git
+cd dqm4hep-online
+mkdir -p build && cd build
+cmake -DINSTALL_DOC=OFF -Dxdrstream_DIR=$PWD/../../xdrstream -DCMAKE_MODULE_PATH=$PWD/../../dqm4hep/cmake -DDQMCore_DIR=$PWD/../../dqm4hep-core -DDQMNet_DIR=$PWD/../../dqm4hep-net ..
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-online cmake"
+    exit 1
+fi
+
+make install VERBOSE=1
+
+if [ $? -ne 0 ]; then
+    echo "Failed to run dqm4hep-online make"
+    exit 1
+fi
+cd ../..
+
+ls -la
+cd ..
